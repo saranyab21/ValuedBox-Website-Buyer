b00069c6975579f87e8160025e78795440ef873d
Aman Chopra <amanchopra.atg@gmail.com>
2021-06-17 12:38:34 +0530

    improvised structure

---
+++ src/pages/Registeration/Registeration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registeration = () => {
+    return (
+        <div>
+            <h1>Registeration</h1>R
+        </div>
+    )
+}
+
+export default Registeration
